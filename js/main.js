8f88a3dfee8970e20ee42deb822b63711a129d37
Nataliia Klapchuk <nataklap@gmail.com>
2024-09-15 06:58:12 +0300

    feat: implement hero section and responsive header with mobile navigation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log('Hello world');
+// Toggle mobile menu
+const navBtn = document.querySelector('.header__nav-trigger');
+const nav = document.querySelector('.nav');
+
+navBtn.addEventListener('click', () => {
+	navBtn.classList.toggle('header__nav-trigger--open');
+	nav.classList.toggle('nav--open');
+});
+
+// Hide Menu on click
+nav.addEventListener('click', (e) => {
+	if (e.target.classList.contains('nav__link')) {
+		navBtn.classList.remove('header__nav-trigger--open');
+		nav.classList.remove('nav--open');
+	}
+});
